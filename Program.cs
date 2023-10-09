516e20794e672ca4b7a1e893964d8444305f5d3b
Levente Major <leventemajor36@gmail.com>
2023-10-09 09:23:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nobeldijasok = new List<Tudos>();
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\orvosi_nobeldijak.txt",
+                Encoding.UTF8
+                );
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                nobeldijasok.Add(new Tudos(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"3.Feladat: Díjazottak száma: {nobeldijasok.Count()} fő");
+            Console.WriteLine($"4.Feladat: Utolsó év: {nobeldijasok.Max(n => n.Ev)}");
+
+            Console.Write("5.Feladat: Kérem adja meg egy ország kódját: ");
+            string orszagKod = Console.ReadLine();
+
+            if (nobeldijasok.Any(n => n.OrszagKod == orszagKod))
+            {
+                if (nobeldijasok.Count(n => n.OrszagKod == orszagKod) == 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("A megadott országból nem volt díjazott!");
+            }
+        }
+    }
+}
---
+++ Tudos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    class Tudos
+    {
+        public int Ev { get; set; }
+        public string Nev { get; set; }
+        public string SzuletesHalalozas { get; set; }
+        public string OrszagKod { get; set; }
+
+        public Tudos(string sor)
+        {
+            var darab = sor.Split(';');
+            this.Ev = int.Parse(darab[0]);
+            this.Nev = darab[1];
+            this.SzuletesHalalozas = darab[2];
+            this.OrszagKod = darab[3];
+        }
+    }
+}
